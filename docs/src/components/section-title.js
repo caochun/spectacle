40fed014caf35b837585ed5a08044d5cf5cadae1
Kylie Stewart <kylie@kyliestewart.tech>
2020-04-01 15:01:55 -0600

    remove typography components, rely on theme styles

--- docs/src/components/section-title.js
+++ docs/src/components/section-title.js
@@ -5,13 +5,10 @@ export const SectionTitle = styled.h2`
   font-size: 2.5rem;
   flex: auto;
   line-height: 1.3;
-  margin: 2rem 0 3rem;
+  margin: 4rem 0;
   width: 100%;
   text-align: center;
-  @media (min-width: 768px) {
-    margin: 2rem 0 6rem;
-  }
-  @media (max-width: 768px) {
-    margin: 4rem 0;
+  @media ${p => p.theme.media.sm} {
+    margin: 2rem 0 ${({ compact }) => (compact ? '2rem' : '6rem')};
   }
 `;
